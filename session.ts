bc3a40fac717b95cbe749ed8e9529d81ab864ccc
yousufsarker <95337943+yousufsarker@users.noreply.github.com>
2023-04-13 15:09:05 +0400

    Create session.ts

---
+++ session.ts
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.18;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract bhola {
+    uint age;
+    function setAge(uint x) public {
+        age =x;
+    }
+    function getAge() public view returns (uint) {
+        return age;
+    }
+}
